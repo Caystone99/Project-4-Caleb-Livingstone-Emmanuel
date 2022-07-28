6e1ec799b9643e41100b293803f87d974b0fbc3f
Caystone99 <emmanuelcaleb546@gmail.com>
2022-07-28 02:37:46 +0100

    init

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '3z1bk3fs36';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'caystone.us.auth0.com',
+  clientId: 'guRjLzkXAVwRFeXHpDRYzAoarH8oLWVb',
+  callbackUrl: 'http://localhost:3000/callback'
+};
